bdd4e2b37e495460ecb5a71c7e28e55045cfb413
chamny20 <dge3179@ajou.ac.kr>
2024-10-09 14:22:19 +0900

    🌈 Update(#2): font and color variable setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,111 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        // color palette
+        'primary-main': '#DE9463',
+        'secondary-main': '#A37D64',
+        'success-main': '#6591FF',
+        'warning-main': '#FFD66A',
+        'error-main': '#FF6A6A',
+        'black-main': '#272222',
+
+        // gray scale
+        'gray-05': '#F4F2F1',
+        'gray-10': '#E9E6E4',
+        'gray-20': '#D0C7C4',
+        'gray-40': '#A79F9C',
+        'gray-60': '#89827F',
+        'gray-80': '#4E4540',
+      },
+      fontSize: {
+        h1: [
+          '2rem', // 32px
+          {
+            fontWeight: 'bold',
+            lineHeight: '150%',
+            letterSpacing: '-2%',
+          },
+        ],
+        h2: [
+          '1.5rem', // 24px
+          {
+            fontWeight: 'regular',
+            lineHeight: '150%',
+            letterSpacing: '-2%',
+          },
+        ],
+        subtitle1: [
+          '1.125rem', //20px
+          {
+            fontWeight: 'bold',
+            lineHeight: '160%',
+            letterSpacing: '-2%',
+          },
+        ],
+        subtitle2: [
+          '1.25rem', //20px
+          {
+            fontWeight: 'regular',
+            lineHeight: '160%',
+            letterSpacing: '-2%',
+          },
+        ],
+        subtitle3: [
+          '1.125rem', //18px
+          {
+            fontWeight: 'bold',
+            lineHeight: '155%',
+            letterSpacing: '-2%',
+          },
+        ],
+        subtitle4: [
+          '1.125rem', //18px
+          {
+            fontWeight: 'regular',
+            lineHeight: '155%',
+            letterSpacing: '-2%',
+          },
+        ],
+        body1: [
+          '1.125rem', //16px
+          {
+            fontWeight: 'bold',
+            lineHeight: '150%',
+            letterSpacing: '-2%',
+          },
+        ],
+        body2: [
+          '1rem', //16px
+          {
+            fontWeight: 'regular',
+            lineHeight: '150%',
+            letterSpacing: '-2%',
+          },
+        ],
+        body3: [
+          '0.875rem', //14px
+          {
+            fontWeight: 'regular',
+            lineHeight: '140%',
+            letterSpacing: '-2%',
+          },
+        ],
+        body4: [
+          '0.75rem', //12px
+          {
+            fontWeight: 'regular',
+            lineHeight: '130%',
+            letterSpacing: '-2%',
+          },
+        ],
+        navigation1: [
+          '0.75rem', //12px
+          {
+            fontWeight: 'semibold',
+            lineHeight: '130%',
+            letterSpacing: '-2%',
+          },
+        ],
       },
       fontFamily: {
         pretendard: ['var(--font-pretendard)'],
