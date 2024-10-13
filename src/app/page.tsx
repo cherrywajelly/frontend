54ee0728799d14bf595335e64574d889a9e09415
chamny20 <dge3179@ajou.ac.kr>
2024-10-14 02:38:50 +0900

    🌈 Update(#1): test button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div>
       Hello World
       <p className="text-red-500 font-bold">test text</p>
+      <button onClick={() => router.push('/login')}>
+        로그인 페이지로 이동
+      </button>
     </div>
   );
 }
