2d2f7f91e0a3848c2ee26de8f514c81168b45680
chamny20 <dge3179@ajou.ac.kr>
2024-10-14 02:38:27 +0900

    📍 Feat(#1): add kakao callback page

---
+++ src/app/api/v1/login/kakao/page.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import React, { useEffect } from 'react';
+
+import { useKakaoToken } from '@/hooks/api/useLogin';
+
+import { useRouter } from 'next/navigation';
+
+const Page = () => {
+  const router = useRouter();
+
+  let kakaoCode: string = '';
+
+  if (typeof window !== 'undefined') {
+    kakaoCode = new URL(window.location.href).searchParams.get('code') || '';
+  }
+
+  const { data, isLoading, error } = useKakaoToken(kakaoCode);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (error) {
+      console.error('Error fetching Kakao token:', error);
+      return;
+    }
+
+    if (data) {
+      router.push('/');
+    } else {
+      //   router.push('/');
+    }
+  }, [data, isLoading, error, router]);
+
+  return isLoading ? (
+    <div className="h-screen">
+      <div className="w-full h-full flex justify-center items-center">
+        {/* <Spinner /> */}
+        기다리는중..
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
+
+  return null;
+};
+
+export default Page;
