45295390fc4da0f365ccc7f41e2b3d683f395685
chamny20 <dge3179@ajou.ac.kr>
2024-10-14 22:58:28 +0900

    📍 Feat(#4): add google login api and hook

--- src/hooks/api/useLogin.ts
+++ src/hooks/api/useLogin.ts
@@ -1,4 +1,4 @@
-import { getKakaoToken } from '@/api/login';
+import { getGoogleLogin, getKakaoToken } from '@/api/login';
 import { useQuery } from '@tanstack/react-query';
 
 export const useKakaoToken = (kakaoCode: string) => {
@@ -8,4 +8,14 @@ export const useKakaoToken = (kakaoCode: string) => {
     enabled: !!kakaoCode,
   });
   return { data, isLoading, error };
+};
+
+export const useGoogleLogin = (googleCode: string) => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['googleLogin', googleCode],
+    queryFn: () => getGoogleLogin(googleCode),
+    enabled: !!googleCode,
+  });
+
+  return { data, error, isLoading };
 };
