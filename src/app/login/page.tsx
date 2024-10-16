539046c2c5edd5dc90db9b112fa7842b6eaabee4
chamny20 <dge3179@ajou.ac.kr>
2024-10-16 19:35:47 +0900

    🌈 Update(#4): update login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,25 +1,16 @@
 'use client';
 
+import GoogleButton from '@/components/login/GoogleButton';
+import KakaoButton from '@/components/login/KakaoButton';
+
 export default function Home() {
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}&response_type=code`;
-
-  const googleUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&response_type=code&scope=email&access_type=offline`;
-
-  const handleGoogleLogin = () => {
-    if (typeof window !== 'undefined') {
-      window.location.href = googleUrl;
-    }
-  };
-
-  const handleKakaoLogin = () => {
-    window.location.href = kakaoURL;
-  };
-
   return (
-    <div>
-      <button onClick={handleKakaoLogin}>카카오로그인하기 button</button>
-      <div>----</div>
-      <button onClick={handleGoogleLogin}>google login button</button>
+    <div className="border border-black p-4">
+      <div>toast 캐릭터 이미지 넣기</div>
+      <div className="flex flex-col gap-4">
+        <KakaoButton />
+        <GoogleButton />
+      </div>
     </div>
   );
 }
