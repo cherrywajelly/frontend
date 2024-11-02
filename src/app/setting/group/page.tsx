72077fcb356eb4754f46e44c8ec9cf710a115a3c
chamny20 <dge3179@ajou.ac.kr>
2024-11-02 21:30:48 +0900

    📍 Feat(#21): add setting-group page

---
+++ src/app/setting/group/page.tsx
@@ -0,0 +1,17 @@
+import TopBar from '@/components/common-components/top-bar';
+
+export default function SettingGroup() {
+  return (
+    <div className="w-full h-lvh">
+      <TopBar
+        // onBack={handleBack}
+        title="마이페이지"
+        // isRight={step === 2 ? 'submit' : false}
+      />
+
+      <div className="h-[calc(100vh-48px)] flex flex-col bg-gray-05">
+        ㄴㄴㄴ
+      </div>
+    </div>
+  );
+}
