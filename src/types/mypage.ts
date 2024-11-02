901d2db9304b3af1cc5b23c45fa06c9ba77338d1
chamny20 <dge3179@ajou.ac.kr>
2024-11-02 21:30:00 +0900

    📍 Feat(#21): add type about mypage

---
+++ src/types/mypage.ts
@@ -0,0 +1,28 @@
+import { StaticImageData } from 'next/image';
+
+export type UserInfoProps = {
+  nickname: string;
+  profileImg: string | StaticImageData;
+  follower: number;
+  following: number;
+  group: number;
+  children?: React.ReactNode;
+};
+
+// showcase props
+export type ShowcaseProps = {
+  isMine: boolean;
+  nickname?: string;
+};
+
+export type MyEventToastItemProps = {
+  image: string | StaticImageData;
+  title: string;
+  date: string;
+};
+
+export type MyGiftToastItemProps = {
+  image: string | StaticImageData;
+  title: string;
+  groupUser: string[];
+};
