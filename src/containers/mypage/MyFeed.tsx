8e8be32c42d3ae40112d055f9cd82284b251f287
chamny20 <dge3179@ajou.ac.kr>
2024-11-02 21:30:21 +0900

    📍 Feat(#21): add mypage and containers of mypage

---
+++ src/containers/mypage/MyFeed.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+
+import MyEventToastItem from '@/components/mypage/MyEventToastItem';
+import MyGiftToastItem from '@/components/mypage/MyGiftToastItem';
+
+import tempImg from '../../../public/images/default-toast.png';
+
+import clsx from 'clsx';
+
+const variants = {
+  common: 'w-full py-2 text-center',
+  default: 'text-gray-40 text-body1',
+  active: 'text-gray-80 text-body1 border-b-2 border-primary-main',
+};
+
+const tempData = [
+  { image: '', title: '타이틀입니당', date: '2020년 4월 3일' },
+  { image: '', title: '타이틀입니당', date: '2020년 4월 3일' },
+  { image: '', title: '타이틀입니당', date: '2020년 4월 3일' },
+];
+
+const tempData2 = [
+  {
+    image: '',
+    title: 'sdfsdf',
+    groupUser: ['chch', 'chaemin', '채민채민', '아아아'],
+  },
+  {
+    image: '',
+    title: '타이틀입니당',
+    groupUser: ['chch', 'chaemin', '채민채민', '아아아'],
+  },
+  {
+    image: '',
+    title: '타이틀입니당',
+    groupUser: ['chch', 'chaemin', '채민채민', '아아아'],
+  },
+  {
+    image: '',
+    title: '타이틀입니당',
+    groupUser: ['chch', 'chaemin', '채민채민', '아아아'],
+  },
+];
+
+export default function MyFeed() {
+  const [activeTab, setActiveTab] = useState<number>(0);
+
+  const handleTabClick = (tab: number) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <>
+      <div className="w-full flex border-b border-gray-10 px-6">
+        <div
+          className={clsx(
+            variants.common,
+            `${activeTab === 0 ? variants.active : variants.default}`,
+          )}
+          onClick={() => handleTabClick(0)}
+        >
+          이벤트 토스트
+        </div>
+
+        <div
+          className={clsx(
+            variants.common,
+            `${activeTab === 1 ? variants.active : variants.default}`,
+          )}
+          onClick={() => handleTabClick(1)}
+        >
+          선물 토스트
+        </div>
+      </div>
+
+      <div className="bg-white p-6 h-full">
+        <div className="flex flex-col gap-4">
+          {activeTab === 0
+            ? tempData.map((item, idx) => {
+                return (
+                  <MyEventToastItem
+                    key={idx}
+                    image={item.image}
+                    title={item.title}
+                    date={item.date}
+                  />
+                );
+              })
+            : tempData2.map((item, idx) => {
+                return (
+                  <MyGiftToastItem
+                    key={idx}
+                    image={item.image}
+                    title={item.title}
+                    groupUser={item.groupUser}
+                  />
+                );
+              })}
+        </div>
+      </div>
+    </>
+  );
+}
