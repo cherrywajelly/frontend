261c1dacc13592495148925733cd263ec9bab8c8
chamny20 <dge3179@ajou.ac.kr>
2024-11-03 15:22:58 +0900

    🌈 Update(#21): apply ui to group-setting form

--- src/containers/setting/AddGroupUser.tsx
+++ src/containers/setting/AddGroupUser.tsx
@@ -1,119 +1,110 @@
-import { ChangeEvent, useRef, useState } from 'react';
-import { FiCamera } from 'react-icons/fi';
+import { Dispatch, SetStateAction, useState } from 'react';
+import {
+  RiCheckboxCircleFill,
+  RiCheckboxBlankCircleLine,
+} from 'react-icons/ri';
 
 import Button from '@/components/common-components/button';
 import Input from '@/components/common-components/input';
 
-import temp from '../../../public/images/default-toast.png';
+import UserListItem from '@/components/search/UserListItem';
 
-import Image from 'next/image';
+import { UserDefaultProps } from '@/types/user';
 
-const tempData = [
-  { img: temp, nickname: '채민이' },
-  { img: temp, nickname: 'chchch' },
-  { img: temp, nickname: '정채연' },
-  { img: temp, nickname: '에스파' },
-  { img: temp, nickname: 'asdf' },
-  { img: temp, nickname: 'cccx' },
-  { img: temp, nickname: 'cswd' },
-  { img: temp, nickname: 'xvew' },
-  { img: temp, nickname: 'asdfaefg' },
-  { img: temp, nickname: 'vdsefs' },
-];
+import { tempUserList } from './GenerateGroup';
 
-export default function AddGroupUser() {
-  const fileInputRef = useRef<HTMLInputElement>(null);
+export type AddGroupUserProps = {
+  setStep: Dispatch<SetStateAction<number>>;
+  selectedUsers: UserDefaultProps[];
+  setSelectedUsers: Dispatch<SetStateAction<UserDefaultProps[]>>;
+};
 
-  const [profileImg, setProfileImg] = useState<string>('');
-  const [groupName, setGroupName] = useState<string>('');
+export default function AddGroupUser({
+  setStep,
+  selectedUsers,
+  setSelectedUsers,
+}: AddGroupUserProps) {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [searchValue, setSearchValue] = useState<string>('');
 
-  const handleFileUploadClick = () => {
-    if (fileInputRef.current) fileInputRef.current.click();
+  const handleCancel = () => {
+    setSearchValue('');
+    setIsFocused(false);
   };
 
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
+  const toggleUserSelection = (item: UserDefaultProps) => {
+    setSelectedUsers((prev) => {
+      if (prev.includes(item)) {
+        return prev.filter((user) => user !== item);
+      } else {
+        return [...prev, item];
+      }
+    });
+  };
 
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        if (reader.result) {
-          setProfileImg(reader.result as string);
-        }
-      };
-      reader.readAsDataURL(file);
-    }
+  const handleSubmit = () => {
+    setStep((prev) => prev + 1);
   };
 
   return (
-    <div className="w-full p-6 flex flex-col gap-6 justify-between bg-gray-05">
-      <div className="flex flex-col items-center">
-        <div className="relative">
-          <Image
-            src={profileImg || temp}
-            alt=""
-            width={120}
-            height={120}
-            className="object-cover rounded-full w-[120px] h-[120px]"
-          />
-          <div
-            onClick={handleFileUploadClick}
-            className="absolute bottom-[-12px] right-0 border-4 border-gray-05 bg-gray-10 w-[40px] h-[40px] rounded-full flex justify-center items-center"
+    <div className="w-full h-full p-6 flex flex-col bg-gray-05">
+      <div
+        className="flex items-center flex-none
+      "
+      >
+        <Input
+          placeholder="검색어를 입력하세요."
+          size="sm"
+          search
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => {
+            if (!searchValue) {
+              setIsFocused(false);
+            }
+          }}
+          className={`transition-all duration-300 ${
+            isFocused ? 'w-[calc(100%-16px)]' : 'w-full'
+          }`}
+        />
+        {isFocused && (
+          <span
+            onClick={handleCancel}
+            className="whitespace-nowrap text-body1 text-gray-40 transition-opacity duration-300 opacity-100"
           >
-            <FiCamera size={20} />
-          </div>
-          <input
-            ref={fileInputRef}
-            type="file"
-            className="hidden"
-            accept="image/*"
-            multiple
-            onChange={handleFileChange}
-          />
-        </div>
+            취소
+          </span>
+        )}
+      </div>
 
-        <div className="w-full mt-1">
-          <span className="text-body1 text-black-main">그룹명</span>
-          <Input
-            placeholder="그룹명을 입력하세요."
-            size="md"
-            className="mt-4"
-            value={groupName}
-            onChange={(e) => setGroupName(e.target.value)}
-          />
-        </div>
-
-        <div className="w-full mt-5">
-          <span className="text-body1 text-black-main">그룹원</span>
-          <div className="flex flex-col gap-4 mt-3 max-h-[370px] overflow-y-auto">
-            {tempData.map((item) => {
-              return (
-                <div className="flex gap-4 items-center" key={item.nickname}>
-                  <Image
-                    src={item.img}
-                    alt=""
-                    width={48}
-                    height={48}
-                    className="object-cover rounded-full"
-                  />
-                  <span className="text-body2 text-black-main">
-                    {item.nickname}
-                  </span>
-                </div>
-              );
-            })}
-          </div>
+      <div className="flex-grow my-6 overflow-y-auto hide-scrollbar">
+        <div className="flex flex-col gap-4">
+          {tempUserList.map((item, idx) => (
+            <UserListItem
+              key={idx}
+              profileImg={item.profileImg}
+              nickname={item.nickname}
+              onClick={() => toggleUserSelection(item)}
+            >
+              {selectedUsers.includes(item) ? (
+                <RiCheckboxCircleFill className="text-primary-main" size={24} />
+              ) : (
+                <RiCheckboxBlankCircleLine className="text-gray-40" size={24} />
+              )}{' '}
+            </UserListItem>
+          ))}
         </div>
       </div>
 
       <Button
         size="md"
-        className="w-full"
-        color={profileImg && groupName ? 'active' : 'disabled'}
-        // onClick={handleSubmit}
-        disabled={!profileImg || !groupName}
+        color={selectedUsers.length ? 'active' : 'disabled'}
+        onClick={handleSubmit}
+        disabled={!selectedUsers.length}
+        className="flex-none mb-4"
       >
-        그룹 생성
+        다음
       </Button>
     </div>
   );
