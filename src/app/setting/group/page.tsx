e33e50c92373ffe9731db775322a98b670316a45
chamny20 <dge3179@ajou.ac.kr>
2024-11-03 15:25:20 +0900

    🌈 Update(#21): update group-setting page by using step for each form

--- src/app/setting/group/page.tsx
+++ src/app/setting/group/page.tsx
@@ -2,20 +2,19 @@
 
 import { useState } from 'react';
 
-import Button from '@/components/common-components/button';
-import Input from '@/components/common-components/input';
 import TopBar from '@/components/common-components/top-bar';
 
+import { UserDefaultProps } from '@/types/user';
+
 import AddGroupUser from '@/containers/setting/AddGroupUser';
+import GenerateGroup from '@/containers/setting/GenerateGroup';
 
-import temp from '../../../../public/images/default-toast.png';
-
-import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 export default function SettingGroup() {
   const [step, setStep] = useState(0);
   const router = useRouter();
+  const [selectedUsers, setSelectedUsers] = useState<UserDefaultProps[]>([]); // 선택된 유저 상태 추가
 
   const handleBack = () => {
     if (step > 0) {
@@ -29,9 +28,15 @@ export default function SettingGroup() {
     <div className="w-full h-lvh">
       <TopBar onBack={handleBack} title="그룹 만들기" />
 
-      <div className="h-[calc(100vh-48px)] flex flex-col gap-1 bg-gray-05">
-        {step === 0 && <AddGroupUser />}
-        {/* {step === 1 && <GiftToastNameForm />} */}
+      <div className="h-[calc(100vh-48px)] flex flex-col bg-gray-05">
+        {step === 0 && (
+          <AddGroupUser
+            setStep={setStep}
+            selectedUsers={selectedUsers}
+            setSelectedUsers={setSelectedUsers}
+          />
+        )}
+        {step === 1 && <GenerateGroup selectedUsers={selectedUsers} />}
       </div>
     </div>
   );
