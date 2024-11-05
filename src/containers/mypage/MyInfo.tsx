2a98dc64bb7fee255ccca08d2de207cf936be60b
chamny20 <dge3179@ajou.ac.kr>
2024-11-05 15:48:33 +0900

    🌈 Update(#30): apply routing about profile-setting

--- src/containers/mypage/MyInfo.tsx
+++ src/containers/mypage/MyInfo.tsx
@@ -4,7 +4,11 @@ import UserInfo from '@/components/mypage/UserInfo';
 
 import tempImg from '../../../public/images/timetoast.png';
 
+import { useRouter } from 'next/navigation';
+
 export default function MyInfo() {
+  const router = useRouter();
+
   return (
     <div className="px-6 py-4">
       {/* user-info */}
@@ -16,7 +20,12 @@ export default function MyInfo() {
         group={10}
       >
         <div className="w-full flex justify-between gap-[10px]">
-          <Button size="sm" className="w-full h-[36px]" color="active">
+          <Button
+            size="sm"
+            className="w-full h-[36px]"
+            color="active"
+            onClick={() => router.push('/setting/profile')}
+          >
             프로필 편집
           </Button>
         </div>
