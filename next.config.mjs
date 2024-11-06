5c3440942d063df5d4b36def24144e21c95713e3
chamny20 <dge3179@ajou.ac.kr>
2024-11-07 00:50:57 +0900

    🌈 Update(#32): apply env about image-domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 import withPWA from 'next-pwa';
 
+const imgHostName = process.env.NEXT_PUBLIC_IMAGE_HOSTNAME;
+
 const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: `axmpikvsv3z9.objectstorage.ap-chuncheon-1.oci.customer-oci.com`,
+        hostname: imgHostName,
       },
     ],
   },
