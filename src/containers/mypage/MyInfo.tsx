30a98c930eda0f2653ef062714d6cb4fee7da3f3
chamny20 <dge3179@ajou.ac.kr>
2024-11-08 01:15:38 +0900

    📍 Feat(#36): apply my-profile api

--- src/containers/mypage/MyInfo.tsx
+++ src/containers/mypage/MyInfo.tsx
@@ -2,34 +2,37 @@ import Button from '@/components/common-components/button';
 
 import UserInfo from '@/components/mypage/UserInfo';
 
-import tempImg from '../../../public/images/timetoast.png';
+import { useGetMyProfile } from '@/hooks/api/useMyPage';
 
 import { useRouter } from 'next/navigation';
 
 export default function MyInfo() {
   const router = useRouter();
+  const { data, isLoading } = useGetMyProfile();
+  console.log(data);
 
   return (
     <div className="px-6 py-4">
-      {/* user-info */}
-      <UserInfo
-        nickname="chaemin"
-        profileImg={tempImg}
-        follower={20}
-        following={30}
-        group={10}
-      >
-        <div className="w-full flex justify-between gap-[10px]">
-          <Button
-            size="sm"
-            className="w-full h-[36px]"
-            color="active"
-            onClick={() => router.push('/setting/profile')}
-          >
-            프로필 편집
-          </Button>
-        </div>
-      </UserInfo>
+      {data && (
+        <UserInfo
+          nickname={data.nickname}
+          profileImg={data.profileUrl}
+          follower={data.followerCount}
+          following={data.followingCount}
+          group={data.teamCount}
+        >
+          <div className="w-full flex justify-between gap-[10px]">
+            <Button
+              size="sm"
+              className="w-full h-[36px]"
+              color="active"
+              onClick={() => router.push('/setting/profile')}
+            >
+              프로필 편집
+            </Button>
+          </div>
+        </UserInfo>
+      )}
     </div>
   );
 }
