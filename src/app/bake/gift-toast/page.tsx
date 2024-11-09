c1594054a4807b5c57af39f31221f166c72d590d
chamny20 <dge3179@ajou.ac.kr>
2024-11-09 17:31:46 +0900

    🌈 Update(#38): add gift-toast-with-choice form

--- src/app/bake/gift-toast/page.tsx
+++ src/app/bake/gift-toast/page.tsx
@@ -4,10 +4,17 @@ import { useState } from 'react';
 
 import TopBar from '@/components/common-components/top-bar';
 
+import {
+  usePostGiftToastFriend,
+  usePostGiftToastGroup,
+  usePostGiftToastMine,
+} from '@/hooks/api/useGiftToast';
+
 import { giftToastDataState, giftToastStepState } from '@/atoms/toastAtom';
 import ToastDecoForm from '@/containers/write-toast/ToastDecoForm';
 import GiftToastNameForm from '@/containers/write-toast/gift-toast/GiftToastNameForm';
 import GiftToastOpenDateForm from '@/containers/write-toast/gift-toast/GiftToastOpenDateForm';
+import GiftToastWithChoiceForm from '@/containers/write-toast/gift-toast/GiftToastWithChoiceForm';
 
 import { useRouter } from 'next/navigation';
 import { useRecoilState, useResetRecoilState } from 'recoil';
@@ -15,6 +22,8 @@ import { useRecoilState, useResetRecoilState } from 'recoil';
 export default function GiftToastPage() {
   const [step, setStep] = useRecoilState(giftToastStepState);
   const resetGiftToastData = useResetRecoilState(giftToastDataState);
+  const [giftData, setGiftData] = useRecoilState(giftToastDataState);
+  console.log(giftData);
 
   const router = useRouter();
 
@@ -29,29 +38,33 @@ export default function GiftToastPage() {
     }
   };
 
+  const { mutate: mutateGiftToastGroup } = usePostGiftToastGroup();
+  const { mutate: mutateGiftToastFriend } = usePostGiftToastFriend();
+  const { mutate: mutateGiftToastMine } = usePostGiftToastMine();
+
+  const handleSubmit = () => {
+    // mutateGiftToastGroup
+  };
+
   return (
     <div className="w-full h-lvh">
       <TopBar
         onBack={handleBack}
         title="선물 토스트 굽기"
-        isRight={step === 2 ? 'submit' : false}
+        isRight={step === 3 ? 'submit' : false}
       />
 
       <div className="h-[calc(100vh-48px)] flex flex-col gap-1 bg-gray-05">
-        {step === 0 && <GiftToastOpenDateForm />}
-        {step === 1 && <GiftToastNameForm />}
-        {step === 2 && (
+        {step === 0 && <GiftToastWithChoiceForm />}
+        {step === 1 && <GiftToastOpenDateForm />}
+        {step === 2 && <GiftToastNameForm />}
+        {step === 3 && (
           <ToastDecoForm
             stepState={giftToastStepState}
             dataState={giftToastDataState}
+            handleSubmit={handleSubmit}
           />
         )}
-        {/* {step === 3 && (
-          <WriteToastForm
-            stepState={giftToastStepState}
-            dataState={giftToastDataState}
-          />
-        )} */}
       </div>
     </div>
   );
