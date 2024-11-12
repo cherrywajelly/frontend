b8d42e8f4ffa19c249a127035d14d73223790d3c
chamny20 <dge3179@ajou.ac.kr>
2024-11-12 19:54:19 +0900

    🌈 Update(#42): apply updated props and types in mypage

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,21 +1,46 @@
 'use client';
 
+import { useMemo } from 'react';
+
 import BottomBar from '@/components/common-components/bottom-bar';
 import TopBar from '@/components/common-components/top-bar';
 
 import Showcase from '@/components/mypage/Showcase';
 
+import { useGetMyShowcase } from '@/hooks/api/useMyPage';
+import { MyShowcaseResponse } from '@/types/api/mypage';
+
 import MyFeed from '@/containers/mypage/MyFeed';
 import MyInfo from '@/containers/mypage/MyInfo';
 
 export default function MyPage() {
+  const { data, isLoading } = useGetMyShowcase();
+
+  const showcaseDataList = useMemo(
+    () =>
+      data?.map(
+        (item) =>
+          ({
+            eventToastId: item.eventToastId,
+            iconUrl: item.iconUrl,
+          }) as MyShowcaseResponse,
+      ) ?? [],
+    [data],
+  );
+
+  const nickname = localStorage.getItem('nickname');
+
   return (
     <div className="w-full h-lvh">
       <TopBar title="마이페이지" isRight="setting" />
 
       <div className="h-[calc(100vh-48px)] flex flex-col bg-gray-05">
         <MyInfo />
-        <Showcase isMine={true} />
+        <Showcase
+          isMine={true}
+          data={showcaseDataList}
+          nickname={nickname as string}
+        />
         <MyFeed />
       </div>
 
