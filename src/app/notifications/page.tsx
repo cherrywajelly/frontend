40c7299f2c7ecc775a07c4a12c08757bfb69dff5
chamny20 <dge3179@ajou.ac.kr>
2024-11-13 18:24:10 +0900

    🌈 Update(#44): add notifications-page

---
+++ src/app/notifications/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import BottomBar from '@/components/common-components/bottom-bar';
+import TopBar from '@/components/common-components/top-bar';
+
+import NotiDefaultItem from '@/components/notifications/NotiDefaultItem';
+import NotiFollowItem from '@/components/notifications/NotiFollowItem';
+import NotiPieceJamItem from '@/components/notifications/NotiPieceJamItem';
+
+export default function NotificationsPage() {
+  return (
+    <div className="w-full h-screen">
+      <TopBar title="알림" isBackBtn={false} />
+
+      <div className="w-full h-[calc(100vh-144px)] flex flex-grow flex-col overflow-y-auto bg-gray-05">
+        <NotiPieceJamItem />
+        <NotiFollowItem />
+        <NotiDefaultItem />
+      </div>
+
+      <BottomBar />
+    </div>
+  );
+}
