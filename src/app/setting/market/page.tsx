4231a43a7a0c6f37c5cd07de9b820192b9b4c8ca
chamny20 <dge3179@ajou.ac.kr>
2024-11-14 16:24:57 +0900

    📍 Feat(#47): apply market-api and connect api-response

--- src/app/setting/market/page.tsx
+++ src/app/setting/market/page.tsx
@@ -9,7 +9,7 @@ export default function IconMarketPage() {
     <div className="w-full h-lvh">
       <TopBar title="아이콘 마켓" />
 
-      <div className="h-[calc(100vh-48px)] flex flex-col bg-gray-05 py-6">
+      <div className="h-[calc(100vh-48px)] flex flex-col bg-white py-6">
         <div className="flex flex-col items-center">
           {/* 탭별 아이콘 리스트*/}
           <IconMarketTabList />
