f6f3a26a5423ea615fc60d56571d9540bbe0aead
chamny20 <dge3179@ajou.ac.kr>
2024-11-14 13:40:48 +0900

    📍 Feat(#44): add apis and hooks about fcm

---
+++ src/hooks/api/useFcm.ts
@@ -0,0 +1,42 @@
+import { NotificationsListResponse, NotiItemResponse } from '@/types/api/fcm';
+
+import {
+  getMoveNotificationsPage,
+  getNotificationsList,
+  putFCMToken,
+} from '@/api/fcm';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
+// fcm 토큰 저장
+export const usePutFCM = () => {
+  const { mutate, isPending, error } = useMutation({
+    mutationFn: (token: string) => putFCMToken(token),
+    onSuccess: () => {},
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+  return { mutate, isPending, error };
+};
+
+// 알림 목록 조회
+export const useGetNotificationsList = () => {
+  const { data, isLoading, error, refetch } = useQuery<
+    NotificationsListResponse[]
+  >({
+    queryKey: ['notificationsList'],
+    queryFn: () => getNotificationsList(),
+    enabled: true,
+  });
+  return { data, isLoading, error, refetch };
+};
+
+// 알림 관련 페이지로 이동
+export const useGetMoveNotificationsPage = (fcmId: number) => {
+  const { data, isLoading, error, refetch } = useQuery<NotiItemResponse>({
+    queryKey: ['moveNotificationsPage'],
+    queryFn: () => getMoveNotificationsPage(fcmId),
+    enabled: true,
+  });
+  return { data, isLoading, error, refetch };
+};
