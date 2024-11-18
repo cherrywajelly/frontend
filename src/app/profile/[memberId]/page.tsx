e1eb19bcc04088c5208719d45b4662a92b13ea10
chamny20 <dge3179@ajou.ac.kr>
2024-11-18 14:04:25 +0900

    🔨 Fix: add ispending logic

--- src/app/profile/[memberId]/page.tsx
+++ src/app/profile/[memberId]/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useMemo, useState } from 'react';
 import BottomBar from '@/components/common-components/bottom-bar';
 import Button from '@/components/common-components/button';
 import CustomSkeleton from '@/components/common-components/skeleton';
+import Spinner from '@/components/common-components/spinner';
 import TopBar from '@/components/common-components/top-bar';
 
 import Showcase from '@/components/mypage/Showcase';
@@ -116,78 +117,84 @@ export default function UserProfilePage({ params }: { params: PageProps }) {
     }
   };
 
+  const isPending = isPendingPostFollowingUser || isPendingDeleteFollowingUser;
+
   return (
     <div className="w-full h-lvh bg-gray-05">
       <TopBar title={data?.nickname} />
 
       <div className="h-[calc(100vh-144px)] flex flex-grow flex-col bg-gray-05 overflow-y-auto">
-        <div className="px-6 py-4">
-          <UserInfo
-            nickname={data?.nickname ?? ''}
-            profileImg={data?.profileUrl ?? tempImg}
-            follower={data?.followerCount ?? 0}
-            following={data?.followingCount ?? 0}
-            group={data?.teamCount ?? 0}
-            isLoading={isLoadingUserProfile}
-          >
-            <div className="w-full flex justify-between gap-[10px]">
-              {isLoadingUserProfile ? (
-                <CustomSkeleton height={36} containerClassName="w-full" />
-              ) : isFollow ? (
-                <Button
-                  size="sm"
-                  className="w-full h-[36px]"
-                  color="disabled"
-                  onClick={handleFollowClick}
-                  disabled={isPendingDeleteFollowingUser}
-                >
-                  팔로잉
-                </Button>
-              ) : (
-                <Button
-                  size="sm"
-                  className="w-full h-[36px]"
-                  color="primary"
-                  onClick={handleFollowClick}
-                  disabled={isPendingPostFollowingUser}
-                >
-                  팔로우
-                </Button>
-              )}
+        {isPending ? (
+          <Spinner />
+        ) : (
+          <>
+            <div className="px-6 py-4">
+              <UserInfo
+                nickname={data?.nickname ?? ''}
+                profileImg={data?.profileUrl ?? tempImg}
+                follower={data?.followerCount ?? 0}
+                following={data?.followingCount ?? 0}
+                group={data?.teamCount ?? 0}
+                isLoading={isLoadingUserProfile}
+              >
+                <div className="w-full flex justify-between gap-[10px]">
+                  {isLoadingUserProfile ? (
+                    <CustomSkeleton height={36} containerClassName="w-full" />
+                  ) : isFollow ? (
+                    <Button
+                      size="sm"
+                      className="w-full h-[36px]"
+                      color="disabled"
+                      onClick={handleFollowClick}
+                      disabled={isPendingDeleteFollowingUser}
+                    >
+                      팔로잉
+                    </Button>
+                  ) : (
+                    <Button
+                      size="sm"
+                      className="w-full h-[36px]"
+                      color="primary"
+                      onClick={handleFollowClick}
+                      disabled={isPendingPostFollowingUser}
+                    >
+                      팔로우
+                    </Button>
+                  )}
+                </div>
+              </UserInfo>
             </div>
-          </UserInfo>
-        </div>
 
-        <Showcase
-          isMine={false}
-          data={showcaseDataList}
-          nickname={data?.nickname ?? ''}
-          isLoading={isLoadingShowcaseData}
-        />
+            <Showcase
+              isMine={false}
+              data={showcaseDataList}
+              nickname={data?.nickname ?? ''}
+              isLoading={isLoadingShowcaseData}
+            />
 
-        <div className="px-6 pt-2 pb-6 flex flex-col">
-          {isLoadingUserProfile ? (
-            <CustomSkeleton height={24} containerClassName="" />
-          ) : (
-            <span className="text-body1 text-gray-80">
-              {data && data.nickname}님이 구운 토스트
-            </span>
-          )}
+            <div className="px-6 pt-2 pb-6 flex flex-col">
+              {isLoadingUserProfile ? (
+                <CustomSkeleton height={24} containerClassName="" />
+              ) : (
+                <span className="text-body1 text-gray-80">
+                  {data && data.nickname}님이 구운 토스트
+                </span>
+              )}
 
-          {!isLoadingUserProfile &&
-          isFollow &&
-          userEventToastDataList.length > 0 ? (
-            <div className="pt-4 flex flex-col gap-4">
-              {userEventToastDataList.map((item, idx) => (
-                <ToastBox
-                  key={item.eventToastId}
-                  title={item.title}
-                  profileImg={item.memberProfileUrl}
-                  nickname={item.nickname}
-                  openDate={item.openedDate}
-                  toastImg={item.icon.iconImageUrl}
-                >
-                  {/* <Button
+              {!isLoadingUserProfile &&
+              isFollow &&
+              userEventToastDataList.length > 0 ? (
+                <div className="pt-4 flex flex-col gap-4">
+                  {userEventToastDataList.map((item, idx) => (
+                    <ToastBox
+                      key={item.eventToastId}
+                      title={item.title}
+                      profileImg={item.memberProfileUrl}
+                      nickname={item.nickname}
+                      openDate={item.openedDate}
+                      toastImg={item.icon.iconImageUrl}
+                    >
+                      {/* <Button
                     size="sm"
                     color="primary"
                     onClick={() =>
@@ -196,38 +203,41 @@ export default function UserProfilePage({ params }: { params: PageProps }) {
                   >
                     잼 바르기
                   </Button> */}
-                  {item.isWritten ? (
-                    <Button size="sm" color="disabled" disabled>
-                      잼을 발랐어요
-                    </Button>
-                  ) : (
-                    <Button
-                      size="sm"
-                      color="primary"
-                      onClick={() => {
-                        router.push(`/event-toast/${item.eventToastId}`);
-                      }}
-                    >
-                      잼 바르기
-                    </Button>
-                  )}
-                </ToastBox>
-              ))}
-            </div>
-          ) : isLoadingUserProfile ? (
-            <CustomSkeleton height={50} containerClassName="mt-4" />
-          ) : (
-            <div className="mt-4 w-full text-center text-body4 bg-white border border-gray-10 p-4 rounded-[10px]">
-              {isFollow ? (
-                <>{data?.nickname}님이 아직 토스트를 굽지 않았어요!</>
+                      {item.isWritten ? (
+                        <Button size="sm" color="disabled" disabled>
+                          잼을 발랐어요
+                        </Button>
+                      ) : (
+                        <Button
+                          size="sm"
+                          color="primary"
+                          onClick={() => {
+                            router.push(`/event-toast/${item.eventToastId}`);
+                          }}
+                        >
+                          잼 바르기
+                        </Button>
+                      )}
+                    </ToastBox>
+                  ))}
+                </div>
+              ) : isLoadingUserProfile ? (
+                <CustomSkeleton height={50} containerClassName="mt-4" />
               ) : (
-                <>
-                  <b>{data?.nickname}</b>님을 팔로우하면 토스트를 볼 수 있어요!
-                </>
+                <div className="mt-4 w-full text-center text-body4 bg-white border border-gray-10 p-4 rounded-[10px]">
+                  {isFollow ? (
+                    <>{data?.nickname}님이 아직 토스트를 굽지 않았어요!</>
+                  ) : (
+                    <>
+                      <b>{data?.nickname}</b>님을 팔로우하면 토스트를 볼 수
+                      있어요!
+                    </>
+                  )}
+                </div>
               )}
             </div>
-          )}
-        </div>
+          </>
+        )}
       </div>
 
       <BottomBar />
