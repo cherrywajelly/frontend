69febef86d50ad5f696ee07ef378453afdfbfcad
chamny20 <dge3179@ajou.ac.kr>
2024-11-24 22:10:27 +0900

    💄 Style(#108): modified layout of premiums page

--- src/containers/premiums/PremiumsInfo.tsx
+++ src/containers/premiums/PremiumsInfo.tsx
@@ -3,15 +3,19 @@ import Button from '@/components/common-components/button';
 import { useGetPremiumsInfo } from '@/hooks/api/usePremiums';
 
 const variants = {
-  title: 'text-gray-80 text-body1',
+  title: 'text-black-main text-body1 w-1/3 text-center break-keep m-auto',
+  text: 'text-gray-80 text-body2 w-1/3 text-center break-keep m-auto',
 };
 
 export default function PremiumsInfo() {
   const { data, isLoading } = useGetPremiumsInfo();
 
+  console.log('data', data);
+
   return (
-    <div className="mt-[60px] h-full border border-blue-500">
+    <div className="mt-[60px] h-full">
       <div className="flex justify-end gap-3 items-center">
+        <div className="h-[70px] w-1/3" />
         <Button className="!h-[70px] w-1/3 rounded-[6px]" color="secondary">
           BASIC
         </Button>
@@ -21,23 +25,27 @@ export default function PremiumsInfo() {
       </div>
 
       {/* info */}
-      <div className="border border-red-500 h-full">
-        <div className="flex">
+      <div className="h-full mt-[48px] flex flex-col gap-8">
+        <div className="flex gap-3">
           <span className={variants.title}>월간 요금</span>
-          <span>월간 요금</span>
-          <span>월간 요금</span>
+          <span className={variants.text}>무료</span>
+          <span className={variants.text}>5,500원</span>
         </div>
 
-        <div className="flex">
-          <span className={variants.title}>월간 요금</span>
-          <span>토스트 조각 내 이미지 개수</span>
-          <span>토스트 조각 내 이미지 개수</span>
+        <div className="h-[1px] border-t border-gray-20" />
+
+        <div className="flex gap-3">
+          <span className={variants.title}>토스트 조각 내 이미지 개수</span>
+          <span className={variants.text}>3개 이하</span>
+          <span className={variants.text}>10개 이하</span>
         </div>
 
-        <div className="flex">
+        <div className="h-[1px] border-t border-gray-20" />
+
+        <div className="flex gap-3">
           <span className={variants.title}>월간 요금</span>
-          <span>임베드 가능 포맷</span>
-          <span>임베드 가능 포맷</span>
+          <span className={variants.text}>이미지</span>
+          <span className={variants.text}>이미지, 영상, 음악</span>
         </div>
       </div>
     </div>
