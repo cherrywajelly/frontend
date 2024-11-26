5ac7165a67ef3ca1a4ad14e11c3c71405cb81bba
chamny20 <dge3179@ajou.ac.kr>
2024-11-26 23:47:53 +0900

    🔨 Fix(#112): add condition

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|assets|manifest.webmanifest|sitemap.xml|robots.txt|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|assets|manifest.webmanifest|sitemap.xml|robots.txt|favicon.ico|/).*)',
   ],
 };
 
@@ -10,8 +10,9 @@ export function middleware(request: NextRequest) {
   const accessToken = request.cookies.get('accessToken');
 
   const isLoginPage = request.nextUrl.pathname === '/login';
+  const isRootPage = request.nextUrl.pathname === '/';
 
-  if (!accessToken && !isLoginPage) {
+  if (!accessToken && !isLoginPage && !isRootPage) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
