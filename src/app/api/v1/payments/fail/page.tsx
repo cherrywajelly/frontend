962d9b88612daf0fd23ff6ee343af6bb312f76eb
chamny20 <dge3179@ajou.ac.kr>
2024-11-30 15:51:59 +0900

    🌈 Update(#126): apply suspense and apply component

--- src/app/api/v1/payments/fail/page.tsx
+++ src/app/api/v1/payments/fail/page.tsx
@@ -1,84 +1,20 @@
 'use client';
 
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { Suspense } from 'react';
 
 import TopBar from '@/components/common-components/top-bar';
 
-import ConfirmDialog from '@/components/alert/ConfirmDialog';
-
-import { usePostUserPaymentsFail } from '@/hooks/api/usePayments';
-
-import { useRouter, useSearchParams } from 'next/navigation';
+import FailArea from '@/containers/payments/FailArea';
 
 export default function Fail() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  const code = searchParams.get('code');
-  const message = searchParams.get('message');
-  const orderId = searchParams.get('orderId') as string;
-
-  const [countdown, setCountdown] = useState<number>(3);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCountdown((prev) => prev - 1);
-    }, 1000);
-
-    const timerId = setTimeout(() => {
-      router.push('/setting/market');
-    }, 3000);
-
-    return () => {
-      clearInterval(intervalId);
-      clearTimeout(timerId);
-    };
-  }, [router]);
-
-  const { mutate, isPending } = usePostUserPaymentsFail();
-
-  useEffect(() => {
-    const init = async () => {
-      mutate(orderId, {
-        onSuccess: (data) => {
-          console.log('data:', data);
-        },
-        onError: (error) => {
-          console.log('Error:', error);
-        },
-      });
-
-      // window.history.replaceState(null, '', window.location.href);
-    };
-    init();
-  }, [mutate]);
-
-  const [isOpen, setIsOpen] = useState<boolean>(true);
-
   return (
-    <div className="w-full h-lvh">
-      <TopBar title="아이콘 마켓" />
-
-      {isOpen && (
-        <ConfirmDialog
-          description={
-            <>
-              <span className="">결제 실패</span>
-            </>
-          }
-          isOpen={isOpen}
-          className="!max-w-[400px]"
-          onClose={() => setIsOpen((prev) => !prev)}
-        >
-          <div className="flex flex-col items-center">
-            <span className="!text-body2">{code}</span>
-            <span className="!text-body2">{message}</span>
-            <span className="!text-body2">
-              <b>{countdown}</b>초 후 마켓 페이지로 돌아갑니다.
-            </span>
-          </div>
-        </ConfirmDialog>
-      )}
-    </div>
+    <>
+      <div className="w-full h-lvh">
+        <TopBar title="아이콘 마켓" />
+        <Suspense>
+          <FailArea />
+        </Suspense>
+      </div>
+    </>
   );
 }
--- src/app/api/v1/payments/success/page.tsx
+++ src/app/api/v1/payments/success/page.tsx
@@ -1,82 +1,19 @@
 'use client';
 
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { Suspense } from 'react';
 
-import Button from '@/components/common-components/button';
 import TopBar from '@/components/common-components/top-bar';
 
-import ConfirmDialog from '@/components/alert/ConfirmDialog';
-
-import { usePostUserPaymentsSuccess } from '@/hooks/api/usePayments';
-
-import { useRouter, useSearchParams } from 'next/navigation';
+import SuccessArea from '@/containers/payments/SuccessArea';
 
 export default function Success() {
-  const searchParams = useSearchParams();
-
-  const paymentKey = searchParams.get('paymentKey') as string;
-  const orderId = searchParams.get('orderId') as string;
-  const amount = searchParams.get('amount');
-  const iconGroupId = searchParams.get('iconGroupId');
-
-  const router = useRouter();
-
-  const { mutate, isPending } = usePostUserPaymentsSuccess();
-
-  const hasMutated = useRef(false);
-
-  useEffect(() => {
-    if (hasMutated.current) return;
-    hasMutated.current = true;
-
-    const init = async () => {
-      mutate(
-        {
-          paymentKey: paymentKey,
-          orderId: orderId,
-          amount: Number(amount),
-        },
-        {
-          onSuccess: (data) => {
-            console.log('data:', data);
-          },
-          onError: (error) => {
-            console.log('Error:', error);
-          },
-        },
-      );
-
-      // window.history.replaceState(null, '', window.location.href);
-    };
-    init();
-  }, [mutate]);
-
-  const [isOpen, setIsOpen] = useState<boolean>(true);
-
   return (
     <div className="w-full h-lvh">
       <TopBar title="아이콘 마켓" />
-      <div className="h-[calc(100vh-48px)] p-6 flex flex-col bg-white py-6">
-        <h1>결제 성공</h1>
-        <div>{`주문 아이디: ${orderId}`}</div>
-        <div>{`결제 금액: ${Number(amount).toLocaleString()}원`}</div>
-      </div>
 
-      {isOpen && (
-        <ConfirmDialog
-          description="결제가 완료되었어요."
-          isOpen={isOpen}
-          className="!max-w-[400px]"
-          onClose={() => setIsOpen((prev) => !prev)}
-        >
-          <Button
-            className="w-full text-white bg-gray-60"
-            onClick={() => router.push(`/setting/market/${iconGroupId}`)}
-          >
-            확인
-          </Button>
-        </ConfirmDialog>
-      )}
+      <Suspense>
+        <SuccessArea />
+      </Suspense>
     </div>
   );
 }
